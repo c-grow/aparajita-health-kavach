06edbd5dc70f5113f76a47d7fa282276a2b6636c
laxmi0411 <lakshmi123gyanvihar@gmail.com>
2026-06-15 11:20:30 +0530

    Add cache buster to stylesheet link in header.php to force update of style.css

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
     <title><?php echo isset($page_title) ? htmlspecialchars($page_title) : "Aparajita Health Kavach | Home Nursing & Elderly Care Patna"; ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=<?php echo filemtime('style.css'); ?>">
     <?php if (isset($extra_css)) echo $extra_css; ?>
 </head>
 <body>
